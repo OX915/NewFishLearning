ece4bb276d7f437b99712c3d1b57c9f987555e5c
Ox915 <409981400@qq.com>
2023-04-08 21:41:20 +0800

    [feat]:test bag

--- demo1.py
+++ demo1.py
@@ -1,1 +1,8 @@
-print("python测试")
+import matplotlib.pyplot as plt
+
+fix, ax = plt.subplots()
+ax.scatter(x=1, y=2, marker="x", label="A") # type: ignore
+ax.scatter(x=2, y=1, marker="+", label="B") # type: ignore
+ax.plot([1, 2, 3], [1, 2, 4], marker="^", label="plot")
+plt.legend()
+plt.show()
